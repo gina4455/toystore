eb418e55eb241a59258cc1732c0a0a213316dd49
gina4455 <113074267+gina4455@users.noreply.github.com>
2025-03-26 21:05:56 -0400

    Deploying to cPanel in this phase

--- includes/database-connection.php
+++ includes/database-connection.php
@@ -1,51 +1,35 @@
-<?php                                             // Opening PHP tag
+<?php
+$type     = 'mysql';                 // Type of database
+$server   = '192.185.2.183';         // Server the database is on
+$db       = 'ginavinc_toystore';     // Name of the database
+$port     = '3306';                  // Port is usually 3306 in Hostgator
+$charset  = 'utf8mb4';               // UTF-8 encoding using 4 bytes of data per character
 
-$type     = 'mysql';                             // Type of database
-$server   = 'yourIPAddress';                    // Server the database is on
-$db       = 'yourcPanelUsername_toystore';     // Name of the database
-$port     = '3306';                           // Port is usually 3306 in Hostgator
-$charset  = 'utf8mb4';                       // UTF-8 encoding using 4 bytes of data per char
+$username = 'ginavinc_user';     // Enter YOUR username here
+$password = 'AirforceRadio445!';         // Enter YOUR password here                                       
 
-$username = 'yourcPanelUsername_user';     // Enter YOUR cPanel username and user here
-$password = 'yourUserPassword';           // Enter YOUR user password here
+$options  = [                        // Options for how PDO works
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];                                                                  // Set PDO options
 
-
-
-// DO *NOT* CHANGE ANYTHING BENEATH THIS LINE
-
-
-// Array containing options for configuring PDO
-$options  = [                        
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,       // Set error mode to throw exceptions
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Set default fetch mode to associative array
-    PDO::ATTR_EMULATE_PREPARES => false,            // Disable emulation of prepared statements
-];
-
-
-$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset"; // Create DSN 
-try {                                                              // Try connecting to the db
-    $pdo = new PDO($dsn, $username, $password, $options);         // Create a new PDO instance
-} 
-catch (PDOException $e) {                  // Catch any exceptions that occur during connection
-    throw new PDOException($e->getMessage(), $e->getCode());    // Re-throw exception
+// DO NOT CHANGE ANYTHING BENEATH THIS LINE
+$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset"; // Create DSN
+try {                                                               // Try following code
+    $pdo = new PDO($dsn, $username, $password, $options);           // Create PDO object
+} catch (PDOException $e) {                                         // If exception thrown
+    throw new PDOException($e->getMessage(), $e->getCode());        // Re-throw exception
 }
 
-/*
- * Executes an SQL query using PDO, optionally binding parameters.
- * 
- * @param PDO $pdo                      An instance of the PDO class.
- * @param string $sql                   The SQL query string.
- * @param array|null $arguments         Optional array of parameters to bind to the SQL query.
- * @return PDOStatement PDOStatement    A PDOStatement object containing the result set.
- */
 function pdo(PDO $pdo, string $sql, array $arguments = null)
     {
-        if (!$arguments) {                   // If no arguments provided
-            return $pdo->query($sql);       // Run SQL query and return PDOStatement object
+        if (!$arguments) {                   // If no arguments
+            return $pdo->query($sql);        // Run SQL and return PDOStatement object
         }
-        $statement = $pdo->prepare($sql);  // If arguments, prepare SQL statement
-        $statement->execute($arguments);  // Bind & execute SQL statement w/provided arguments
-        return $statement;               // Return PDOStatement object
+        $statement = $pdo->prepare($sql);    // If arguments prepare statement
+        $statement->execute($arguments);     // Execute statement
+        return $statement;                   // Return PDOStatement object
     }
-                                          
-// Closing PHP tag  ?>                                             
+
+?>                                              
